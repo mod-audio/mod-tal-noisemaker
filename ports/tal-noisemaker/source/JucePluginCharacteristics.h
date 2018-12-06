c9764602af1ddd5416b55eab6e157d87f49389fe
BramGiesen <giesenbram@gmail.com>
2018-12-06 10:44:38 +0100

    Changed statestring

--- ports/tal-noisemaker/source/JucePluginCharacteristics.h
+++ ports/tal-noisemaker/source/JucePluginCharacteristics.h
@@ -45,7 +45,7 @@
 #define JucePlugin_WantsLV2Latency          0
 #define JucePlugin_WantsLV2Presets          1
 #define JucePlugin_WantsLV2State            1
-#define JucePlugin_WantsLV2StateString      1
+#define JucePlugin_WantsLV2StateString      0
 #define JucePlugin_WantsLV2TimePos          1
 
 #endif
