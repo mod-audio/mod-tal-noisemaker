46b015c0291dee80189b7a335e39425788e231a4
BramGiesen <giesenbram@gmail.com>
2019-10-11 11:58:42 +0200

    Fixed typo

--- ports/tal-noisemaker/source/Engine/AudioUtils.h
+++ ports/tal-noisemaker/source/Engine/AudioUtils.h
@@ -301,7 +301,7 @@ public:
 
         switch(param)
         {
-        case VOICES: numItems = MAX_VOICES; break;
+        case VOICES: numItems = PMAX_VOICES; break;
         case PORTAMENTOMODE: numItems = 3; break;
         case LFO1DESTINATION: numItems = 8; break;
         case LFO2DESTINATION: numItems = 8; break;
